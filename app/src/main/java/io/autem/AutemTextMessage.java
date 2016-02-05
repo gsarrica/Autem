5aec52c428cb0205b8f22065cdd3fa43be0d4581
Gregory Sarrica <gsarrica@medicalpaymentexchange.com>
2016-02-05 16:20:45 -0500

    add copyright

--- app/src/main/java/io/autem/AutemTextMessage.java
+++ app/src/main/java/io/autem/AutemTextMessage.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2016 Gregory Sarrica and Kyle Chaplin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.autem;
 
 import org.joda.time.LocalDateTime;
